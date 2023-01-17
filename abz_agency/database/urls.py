95d8ec97e3117324c2303c264815447c1eae082a
karavanets <dront22rus@mail.ru>
2023-01-17 20:25:33 +0700

    fix name path

--- abz_agency/database/urls.py
+++ abz_agency/database/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', employee, name='home'),
-    path('search/', search_results, name='search_results'),
+    path('search/', search_results, name='search'),
 ]
