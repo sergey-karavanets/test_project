cb8177ada6b6e815f2c81569d443f045628d064e
karavanets <dront22rus@mail.ru>
2023-01-18 15:12:09 +0700

    update title 'Searching results'

--- abz_agency/database/views.py
+++ abz_agency/database/views.py
@@ -18,4 +18,4 @@ def search_results(request):
             Q(position__icontains=query) |
             Q(employment_date__icontains=query) |
             Q(salary__icontains=query))
-    return render(request, 'database/search.html', {'title': 'Search', 'data': data})
+    return render(request, 'database/search.html', {'title': 'Searching results', 'data': data})
