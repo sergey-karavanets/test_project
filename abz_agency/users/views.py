37d3ba078942497915441b473418a18afc788cf3
karavanets <dront22rus@mail.ru>
2023-01-18 16:40:52 +0700

    update data in views.py

--- abz_agency/users/views.py
+++ abz_agency/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -8,12 +9,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Создан аккаунт {username}!')
-            return redirect('home')
+            messages.success(request, f'Ваш аккаунт создан: можно войти на сайт.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
