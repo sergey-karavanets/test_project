fb675c49f303127c11c43c7b176e1423cd044a78
karavanets <dront22rus@mail.ru>
2023-02-03 18:46:06 +0700

    add decorator '@login_required' in views.py

--- abz_agency/database/views.py
+++ abz_agency/database/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .models import *
 from django.db.models import Q
-from .tree import EmployeeTree
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def employee(request):
     data = Employee.objects.all()
     return render(request, 'database/index.html', {'title': 'Employee information',
                                                    'data': data})
 
 
+@login_required
 def search_results(request):
     query = request.GET.get('q')
     if query is None:
@@ -24,6 +26,7 @@ def search_results(request):
                                                     'data': data})
 
 
+@login_required
 def tree(request):
     return render(request, "database/tree.html", {'title': 'Employee Tree',
                                                   'employees': Employee.objects.all()})
